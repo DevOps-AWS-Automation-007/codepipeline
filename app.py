177a7979693f2bd16b579fb95c6e151e3ac9c2dd
vijaykanth1729 <personalvijay007@gmail.com>
2022-03-03 19:02:07 +0530

    updated app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ load_dotenv()
 
 @app.route("/")
 def hello():
-    return f"<h1>Hello World! deployed to AWS {os.getenv('data')}</h1>"
+    return f"<h1>Hello World! deployed to AWS BY ---> {os.getenv('data')}</h1>"
 
 if __name__ == '__main__':
     app.run(host=os.getenv('host'), port=os.getenv('port'), debug=False)
